5e0b5e955ff806466b81c07b3dcc064a78901504
juantejedavz <juan.tejedavz@udlap.mx>
2015-08-28 12:34:05 -0500

    Sorting

---
+++ Sort.js
@@ -0,0 +1,15 @@
+lista=[3,4,5,6,1];
+
+temp=lista[lista.length-1];
+
+i=lista.length-2;
+
+while(i>=0 && temp<lista[i]){
+	lista[i+1]=lista[i]
+	i--;
+	console.log(lista);
+}
+
+lista[i]=temp;
+
+console.log(lista);
